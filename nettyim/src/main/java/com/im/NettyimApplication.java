6568faffc93fa4abfd7bb4e5b0ea72a647f47696
zsk-com <1263294312@qq.com>
2020-08-11 18:10:36 +0800

    SpringBoot与Netty的整合

---
+++ nettyim/src/main/java/com/im/NettyimApplication.java
@@ -0,0 +1,45 @@
+package com.im;
+
+import com.im.server.http.HttpServer;
+import io.netty.channel.ChannelFuture;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * CommandLineRunner 为项目启动后执行的逻辑
+ */
+@SpringBootApplication
+public class NettyimApplication implements CommandLineRunner {
+    //注入启动http启动了类
+    @Autowired
+    private HttpServer server;
+    //获取配置文件中的端口
+    @Value("${netty.port}")
+    private int port;
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyimApplication.class, args);
+    }
+
+    /**
+     * untime.getRuntime().addShutdownHook
+     * 在jvm关闭时会查看是否设置addShutdownHook
+     * 如果有会先执行addShutdownHook()中的逻辑在关闭
+     *
+     * @param args
+     * @throws Exception
+     */
+    @Override
+    public void run(String... args) throws Exception {
+        ChannelFuture f = server.server(port);
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                NettyimApplication.this.server.close();
+            }
+        });
+        f.channel().closeFuture().sync();
+    }
+}
---
+++ nettyim/src/test/java/com/im/nettyim/NettyimApplicationTests.java
@@ -0,0 +1,14 @@
+package com.im.nettyim;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
